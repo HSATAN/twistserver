0e37815adc84bb16a48945bb83eb42945674fd03
edison <2499090390@qq.com>
2017-10-24 19:42:40 +0800

    添加认证函数

--- server.py
+++ server.py
@@ -28,9 +28,9 @@ class App(Resource):
 class Auth(Resource):
     isLeaf = True
     def render_GET(self, request):
-        return json.dumps({"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 11111})
+        return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 11111})
     def render_POST(self, request):
-        return json.dumps({"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 11111})
+        return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 11111})
 class UserInfo(Resource):
     isLeaf = True
     def render_GET(self, request):
