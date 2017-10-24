26fe2f5da03363f7d6546a974ce3957d33e496a3
edison <2499090390@qq.com>
2017-10-24 21:49:01 +0800

    fix

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # _*_ coding:utf-8 _*_
-
+from __future__ import print_function
 from twisted.internet import reactor
 from twisted.web.resource import Resource
 from twisted.web.server import Site
@@ -32,12 +32,17 @@ class Auth(Resource):
         return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 11111})
     def render_POST(self, request):
         phone_number = request.args.get("phone_number",0)
-        if not phone_number:
-            return  json.dumps({"code":20002,"msg": "参数错误"})
-        user = PsyBase.find_user(phone_number)
-        if not user:
-            return  json.dumps({"code": 20003, "msg": "用户未找到"})
-        return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 10000})
+        print(phone_number)
+        try:
+            if not phone_number:
+                return  json.dumps({"code":20002,"msg": "参数错误"})
+            user = PsyBase.find_user(phone_number)
+            if not user:
+                return  json.dumps({"code": 20003, "msg": "用户未找到"})
+            return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 10000})
+        except Exception as e:
+            print(e)
+            return  json.dumps({"code":2004,"msg": "服务器错误"})
 class UserInfo(Resource):
     isLeaf = True
     def render_GET(self, request):
