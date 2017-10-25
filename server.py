93cda47fe1a58e98f97d2c84d8f8661aeab38ec1
huangkaijie <huangkaijie@huangkaijiedeiMac.local>
2017-10-25 11:10:01 +0800

    fix

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ class Auth(Resource):
             user = PsyBase.find_user(phone_number)
             if not user:
                 return  json.dumps({"code": 20003, "msg": "用户未找到"})
-            return json.dumps({"number": 999,"name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 10000})
+            return json.dumps({"phone_number": 999, "name": "黄开杰", "age": 25, "id": 1, "password": "123456","code": 10000})
         except Exception as e:
             print(e)
             return  json.dumps({"code":2004,"msg": "服务器错误"})
