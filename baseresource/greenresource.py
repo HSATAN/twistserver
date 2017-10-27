339f01ed4ad793db2cdda65e224b8efa0f405067
edison <2499090390@qq.com>
2017-10-28 02:45:04 +0800

    添加redis 缓存

--- baseresource/greenresource.py
+++ baseresource/greenresource.py
@@ -4,14 +4,15 @@ from twisted.web.resource import Resource
 from dataredis.redisconn import BaseRedis
 import json
 from config import errorcode
+
 def ban_user():
-
-
+    #   返回异常信息
     return json.dumps({"code": errorcode.TYPES.BAN,"msg":"对不起，由于你操作异常，我们暂时不能为你提供服务"})
 
 
 
 def pre_handle_decotor(func):
+    #    记录和检测用户的访问频率，如果用户访问次数超过限制，则不返回有效信息
     def handler(*args, **kwargs):
         ip = '00000000'
         try:
