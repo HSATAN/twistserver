6bc057e61ed55b41fb0ba0e3a0c9d4ebc87022bc
edison <2499090390@qq.com>
2017-10-28 02:32:39 +0800

    添加redis 缓存

--- baseresource/greenresource.py
+++ baseresource/greenresource.py
@@ -2,11 +2,23 @@
 from __future__ import print_function
 from twisted.web.resource import Resource
 from dataredis.redisconn import BaseRedis
+import json
+from config import errorcode
+def ban_user():
+
+
+    return json.dumps({"code": errorcode.TYPES.BAN})
+
+
+
 def pre_handle_decotor(func):
     def handler(*args, **kwargs):
         ip = '00000000'
         try:
             ip = args[1].getHeader('X-Real-IP').strip('')
+            if int(BaseRedis.r.get(ip)) > 1000:
+                print("此ip已经被限制访问 %s" % ip)
+                return ban_user()
         except Exception as e:
 
             print(e)
@@ -45,6 +57,7 @@ class BaseResource(Resource):
     @pre_handle_decotor
     def real_POST(self, request):
         pass
+
 
 
 
