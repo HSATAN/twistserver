b2a613bb1c4e68a537ab0bf3a6cf9fa7a293b9a5
edison <2499090390@qq.com>
2017-10-28 23:44:05 +0800

    fix

--- api/weixin/auth.py
+++ api/weixin/auth.py
@@ -1,5 +1,6 @@
 # _*_ coding:utf-8 _*_
 from baseresource.greenresource import BaseResource
+from lxml import etree
 
 class AuthWeiXin(BaseResource):
 
@@ -7,5 +8,23 @@ class AuthWeiXin(BaseResource):
         BaseResource.__init__(self)
 
     def real_GET(self, request):
-        return "微信验证"
-        pass
+        print(dir(request))
+        print(request.content.read())
+        return "weixinyanzhegn"
+        receiveData = request.body.decode('utf8')
+        print(receiveData)
+        data = etree.fromstring(receiveData)
+        ToUserName = data.find('ToUserName').text
+        FromUserName = data.find('FromUserName').text
+        CreateTime = data.find('CreateTime').text
+        # Content = data.find('Content').text
+        Content = '我爱熊麟茹'
+        # print(receiveData)
+        message = '''<xml>
+        <ToUserName><![CDATA[{0}]]></ToUserName>
+        <FromUserName><![CDATA[{1}]]></FromUserName>
+        <CreateTime>{2}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[{3}]]></Content>
+        </xml>'''.format(FromUserName, ToUserName, CreateTime, Content)
+        return message
