38d206c0fa6d6f6e7194e10fcf52993f49356c2b
edison <2499090390@qq.com>
2017-11-10 21:27:32 +0800

    fix

--- api/api.py
+++ api/api.py
@@ -11,3 +11,5 @@ class Api(BaseResource):
 
 
     pass
+
+
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 # _*_ coding:utf-8 _*_
+
+with open('hlm') as f:
+    data = f.read()
+    data = data.split('\n')
+    back_data = list(set(data))
+print(back_data[1])
+print(len(back_data))
