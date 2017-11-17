56f6675355b87c5718ea6a712e1796a807cd355b
huangkaijie <2499090390@qq.com>
2017-11-17 09:56:25 +0800

    fix

---
+++ config/mysql_config.py
@@ -0,0 +1,6 @@
+# _*_ coding: utf8 _*_
+MYSQL_HOST = '47.93.5.189'
+MYSQL_PORT = 3306
+MYSQL_PASSWORD = 'edison'
+MYSQL_USER = 'root'
+MYSQL_DATABASE = 'serchdocment'
---
+++ database/mysqldb.py
@@ -0,0 +1,40 @@
+# _*_ coding: utf8 _*_
+
+import config.mysql_config as config
+import pymysql
+import logging
+
+
+class MysqlDB(object):
+
+    conn = pymysql.connect(host=config.MYSQL_HOST,
+                           port=config.MYSQL_PORT,
+                           password=config.MYSQL_PASSWORD,
+                           db=config.MYSQL_DATABASE,
+                           user=config.MYSQL_USER,
+                           charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor)
+    cursor = conn.cursor()
+
+    @classmethod
+    def insert(cls, operation):
+        try:
+            cls.cursor.execute(operation)
+            cls.commit()
+        except Exception as e:
+            logging.error("insert error: %s" % e)
+            cls.rollback()
+            cls.commit()
+
+    @classmethod
+    def close(cls):
+        cls.cursor.close()
+        cls.conn.close()
+
+    @classmethod
+    def commit(cls):
+        cls.conn.commit()
+
+    @classmethod
+    def rollback(cls):
+        cls.conn.rollback()
