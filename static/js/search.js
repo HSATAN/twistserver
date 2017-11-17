a44e7be6a50ebb524ff9588a5e505be8c878cdff
edison <2499090390@qq.com>
2017-11-17 23:45:55 +0800

    fix

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,18 @@
 function load_document() {
-    alert("-----------")
-    var flag = false
-    $(document).ready(function () {
-        $("#su").click(function () {
-            htmlobj = $.ajax({url: "/search",
-                async: false,
-            }).done(function () {
-                flag = false
-            });
-        });
-    });
-    return flag;
+    alert("--------------------------------")
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function () {
+        if (xmlhttp.readyState==4)
+        {
+            if(xmlhttp.status==200)
+            {
+                alert("加载完成")
+                        document.getElementById("result").innerText="ceshi"
+
+            }
+        }
+    };
+    xmlhttp.open("GET","/search?word=huagnkaijie",false);
+    xmlhttp.send(null);
 }
