9943fc2204f431837a12ad85016dfae5361d81c7
edison <2499090390@qq.com>
2017-11-19 18:33:23 +0800

    fix

---
+++ practice/my_metaclass.py
@@ -0,0 +1,36 @@
+# _*_ coding:utf-8 _*_
+
+def choose_class(classname=None, parent=(),attr={}):
+    attrs = ((name, value) for name, value in attr.items() if not name.startswith('__'))
+    uppersttrs = dict((name.upper(), value) for name, value in attrs)
+    return type(classname, parent,uppersttrs)
+
+class Foo(object):
+
+    __metaclass__ =  choose_class
+
+    fib = "这个属性名会被转为大写"
+
+foo = Foo()
+print(foo.FIB)
+print(hasattr(foo, "FIB"))
+foo.fib = "这是新属性"
+print(foo.fib)
+print(foo.__class__)
+
+
+class UpperAttr(object):
+    # def __new__(cls, name, bases, attrdict,  *args, **kwargs):
+    #     attrs = ((name, value) for name, value in attrdict.items() if  not name.startswith('__'))
+    #     upperattrs = dict((name.upper(), value) for name, value in attrs)
+    #     return super(UpperAttr, cls).__new__(cls, name,bases,upperattrs, *args, **kwargs)
+    # lll = "zhe shi xiao xie"
+    def __new__(cls, *args, **kwargs):
+        print('__new__')
+        return object.__new__(cls,*args, **kwargs)
+    def __call__(self, *args, **kwargs):
+        print('__call__')
+        return "nihao"
+
+ins = UpperAttr()
+
