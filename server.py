df6c8dcf48ccf172297a0056876b61fa49d17cc7
edison <2499090390@qq.com>
2018-03-26 19:28:46 +0800

    update

--- server.py
+++ server.py
@@ -113,7 +113,7 @@ class third(Resource):
 
     isLeaf = True
     def render_GET(self,request):
-        return bytes("third", encoding='utf8')
+        return bytes({'name': '黄开杰'}, encoding='utf8')
 
 if __name__ == '__main__':
     logfile = 'log'
